baad02cfd8c6cdbb36c7305d31421e38bc1ba10a
mareks.riska <mareks.riska@autentica.lv>
2020-11-05 23:24:26 +0200

    jobListInsideFolder

---
+++ jobListInsideFolder.groovy
@@ -0,0 +1,20 @@
+import com.cloudbees.hudson.plugins.folder.Folder;
+
+/**
+ * Returns a list of job full names inside the found folder and its subfolders
+ **/
+def jobListInsideFolder(folderName){
+    // Gets parent folder of current running job
+    def parentFolder = getContext(Job.class).parent
+    
+    // Finds top folder of current running job
+    while(!(parentFolder.parent instanceof Hudson)){
+        parentFolder = parentFolder.parent
+    }
+    
+    // Looks for specified folder inside top folder
+    def folder = parentFolder.getAllItems(Folder.class).find { it.fullName.contains(folderName) };
+    
+    // Returns all jobs inside found folder and subfolders
+    return folder?.getAllItems(Job.class).collect { it.fullName } 
+}
